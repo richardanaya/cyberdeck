e87cf40e851de0c05edefccc3d123641c5548b5e
Richard Anaya <richard.anaya@gmail.com>
2021-10-17 09:34:30 -0700

    expose important types

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,14 @@ use tokio::sync::mpsc;
 use webrtc::api::interceptor_registry::register_default_interceptors;
 use webrtc::api::media_engine::MediaEngine;
 use webrtc::api::APIBuilder;
-use webrtc::data::data_channel::data_channel_init::RTCDataChannelInit;
-use webrtc::data::data_channel::data_channel_message::DataChannelMessage;
+pub use webrtc::data::data_channel::data_channel_init::RTCDataChannelInit;
+pub use webrtc::data::data_channel::data_channel_message::DataChannelMessage;
 pub use webrtc::data::data_channel::data_channel_state::RTCDataChannelState;
 pub use webrtc::data::data_channel::RTCDataChannel;
 use webrtc::peer::configuration::RTCConfiguration;
 use webrtc::peer::ice::ice_server::RTCIceServer;
 use webrtc::peer::peer_connection::RTCPeerConnection;
-use webrtc::peer::peer_connection_state::RTCPeerConnectionState;
+pub use webrtc::peer::peer_connection_state::RTCPeerConnectionState;
 use webrtc::peer::sdp::session_description::RTCSessionDescription;
 
 pub struct Configuration {
